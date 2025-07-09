d7156c52ca4dbf4afb7f0d3f2b9a13a857fca103
shyam01999 <shyamsundarsahoo1998@gmail.com>
2025-07-09 06:54:04 +0530

    upload file recreate

--- middlewares/uploadMiddleware.js
+++ middlewares/uploadMiddleware.js
@@ -1,5 +1,11 @@
 const multer = require("multer");
 const path = require("path");
+const fs = require("fs");
+const uploadPath = path.join(__dirname, "..", "uploads");
+
+if (!fs.existsSync(uploadPath)) {
+  fs.mkdirSync(uploadPath, { recursive: true });
+}
 
 //set our multer storage
 const storage = multer.diskStorage({
